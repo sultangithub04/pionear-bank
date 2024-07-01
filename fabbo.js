e0904684078d8a913d5195b6cb8d40c188ac01cd
sultangithub04 <sult_04@yahoo.com>
2024-07-01 23:58:11 +0600

    lern fibonacci

--- fabbo.js
+++ fabbo.js
@@ -12,17 +12,46 @@
 // console.log(result);
 
 
+// function fibonaci(n){
+//     if(n==0){
+//         return 0;
+//     }
+//     elseif(n==1){
+//         return 1;
+//     }
+//     else{
+//         return fibonaci(n-1)+fibonaci(n-2);
+//     }
+
+// }
+// var result =fibonaci(10);
+// console.log(result);
+        // function primeNumber(n){
+        //     for(let i=2; i<n; i++){
+        //     if(n%i==0){
+        //         return "not a prime number"
+        //     }
+        //     return "prime number"
+        //     }
+
+        // }
+        // var result =primeNumber(18)
+        // console.log(result)
+
 function fibonaci(n){
     if(n==0){
-        return 0;
+        return [0];
     }
-    elseif(n==1){
-        return 1;
+    else if(n==1){
+        return [0,1];
     }
     else{
-        return fibonaci(n-1)+fibonaci(n-2);
+        var fibo= fibonaci(n-1);
+        var nexelement= fibo[n-1]+fibo[n-2];
+        fibo.push(nexelement);
+        return fibo;
     }
-
 }
-var result =fibonaci(10);
-console.log(result);
+    var result=fibonaci(10);
+    console.log(result);
+
